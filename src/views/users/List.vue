cdb7de8630fdb77d17d236d5abeb38e7efcdb1ef
chen <1398512906@qq.com>
2018-09-06 10:35:35 +0800

    分配角色等

--- src/views/users/List.vue
+++ src/views/users/List.vue
@@ -69,7 +69,7 @@
         </template>
       </el-table-column>
       <el-table-column
-        width="190"
+      width="190"
         label="操作">
         <template slot-scope="scope">
           <!-- 通过scope.$index可以获取到当前行的索引 -->
@@ -159,39 +159,45 @@
       </div>
     </el-dialog>
 
-    <!-- 分配角色 -->
+    <!-- 分配角色的对话框 -->
     <el-dialog
       title="分配角色"
       :visible.sync="setRoleDialogFormVisible"
       @close="handleClose">
       <el-form
-      label-width="90px"
-      :model="formData">
-      <el-form-item label="用户名">
-        {{ formData.username}}
-      </el-form-item>
-      <el-form-item label="请选择角色">
-      <el-select v-model="currentRoled" placeholder="请选择">
-        <el-option
-        label="请选择"
-        :value="-1" disabled>
-        </el-option>
-        <el-option
-          label = "小学"
-          value = "-1">
-        </el-option>
-      <!-- <el-option
-          v-for="item in options"
-          :key="item.value"
-          :label="item.label"
-          :value="item.value">
-          </el-option> -->
-        </el-select>
-      </el-form-item>
+        label-width="100px"
+        :model="formData">
+        <el-form-item label="用户名">
+          {{ formData.username }}
+        </el-form-item>
+        <el-form-item label="请选择角色">
+          <!-- 下拉框, currentRoleId默认为-1 -->
+          <!-- {{ currentRoleId }} -->
+          <el-select v-model="currentRoleId" placeholder="请选择">
+            <el-option
+              label="请选择"
+              :value="-1" disabled>
+            </el-option>
+            <el-option
+              v-for="item in options"
+              :key="item.id"
+              :label="item.roleName"
+              :value="item.id">
+            </el-option>
+            <!-- <el-option
+              label="大学"
+              value="1">
+            </el-option>
+            <el-option
+              label="小学"
+              value="2">
+            </el-option> -->
+          </el-select>
+        </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="setRoleDialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="handleEdit">确 定</el-button>
+        <el-button type="primary" @click="handleSetRole">确 定</el-button>
       </div>
     </el-dialog>
   </el-card>
@@ -217,6 +223,7 @@ export default {
       addUserDialogFormVisible: false,
       // 控制修改用户对话框的显示或隐藏
       editUserDialogFormVisible: false,
+      // 控制分配角色的对话框显示或者隐藏
       setRoleDialogFormVisible: false,
       // 绑定的表单对象
       formData: {
@@ -236,8 +243,10 @@ export default {
           { min: 3, max: 8, message: '长度在 3 到 8 个字符', trigger: 'blur' }
         ]
       },
-      //绑定下拉框
-      currentRoled: -1
+      // 绑定下拉框
+      currentRoleId: -1,
+      // 绑定下拉框的数据（所有的角色）
+      options: []
     };
   },
   created() {
@@ -367,6 +376,8 @@ export default {
       for (let key in this.formData) {
         this.formData[key] = '';
       }
+      // 重置下拉框中默认显示的项
+      this.currentRoleId = -1;
     },
     // 点击编辑按钮，打开修改用户的对话框
     handleOpenEditDialog(user) {
@@ -399,11 +410,40 @@ export default {
       } else {
         this.$message.error(msg);
       }
-
     },
-    //点击分配角色, 打开分配的对话框
-    handleOpenSetRoleDialog() {
+    // 点击分配角色，打开分配角色的对话框
+    async handleOpenSetRoleDialog(user) {
       this.setRoleDialogFormVisible = true;
+      this.formData.username = user.username;
+      // 发送请求获取所有的角色
+      const response = await this.$http.get('roles');
+      this.options = response.data.data;
+
+      // 设置当前用户默认的角色
+      // console.log(user);
+      // this.currentRoleId
+      // 根据用户id，查询用户信息（找到当前用户对应的角色id）
+      const res = await this.$http.get(`users/${user.id}`);
+      // console.log(res.data);
+      this.currentRoleId = res.data.data.rid;
+      // 记录用户id
+      this.formData.id = user.id;
+    },
+    // 设置用户角色
+    async handleSetRole() {
+      // put  users/:id/role     请求体需要rid
+      const response = await this.$http.put(`users/${this.formData.id}/role`, {
+        rid: this.currentRoleId
+      });
+      const { meta: { msg, status } } = response.data;
+      if (status === 200) {
+        // 成功
+        this.$message.success(msg);
+        this.setRoleDialogFormVisible = false;
+      } else {
+        // 失败
+        this.$message.error(msg);
+      }
     }
   }
 };
